c9b5a4e7c2d5bd0b4ec651ab1f59f7ebf746a765
Ricky Clegg <me@rickyclegg.com>
2013-03-03 18:10:59 +0000

    Added line spacing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+
     grunt.loadNpmTasks('grunt-docco');
 
     grunt.registerTask('default', ['jshint:beforeconcat', 'concat:dist', 'jshint:afterconcat', 'uglify']);
